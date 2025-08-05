948bd5eb77974ee359e84a83b7870be871c99c4d
Ling chen <145187919+CHen12313@users.noreply.github.com>
2025-08-05 10:44:51 +0800

    Update config.py

--- Generator/config.py
+++ Generator/config.py
@@ -1,11 +1,7 @@
 import os
 
-#NEWS_API_KEY = os.environ.get("NEWS_API") #set in project "secrets"
-#OPENAI_API_KEY = os.environ.get("OPENAI_API")
-
-NEWS_API_KEY = "468310dccfbf4a668d97646431efdf35"
-#NEWS_API_KEY = "72a07184dc6c4c3aa8b4aa6bba0d53bc"
-OPENAI_API_KEY = "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"
+NEWS_API_KEY = os.environ.get("NEWS_API") #set in project "secrets"
+OPENAI_API_KEY = os.environ.get("OPENAI_API")
 
 # News Categories
 TMT_CATEGORIES = [
